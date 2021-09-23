70ec2ad1ecd075e1019793af769cfb343ab85b1c
elias <elias.damisch@gmail.com>
2021-09-23 20:45:38 +0200

    excersice DOM Selectors

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -1,40 +1,70 @@
-let val;
+// let val;
 
-val = document;
-val = document.all;
-val = document.all[2];
-val = document.all.length;
-val = document.head;
-val = document.body;
-val = document.doctype;
-val = document.domain;
-val = document.URL;
-val = document.characterSet;
-val = document.contentType;
+// val = document;
+// val = document.all;
+// val = document.all[2];
+// val = document.all.length;
+// val = document.head;
+// val = document.body;
+// val = document.doctype;
+// val = document.domain;
+// val = document.URL;
+// val = document.characterSet;
+// val = document.contentType;
 
-val = document.forms;
-val = document.forms[0];
-val = document.forms[0].id;
-val = document.forms[0].method;
-val = document.forms[0].action;
+// val = document.forms;
+// val = document.forms[0];
+// val = document.forms[0].id;
+// val = document.forms[0].method;
+// val = document.forms[0].action;
 
-val = document.links;
-val = document.links[0];
-val = document.links[0].id;
-val = document.links[0].className;
-val = document.links[0].classList[0];
+// val = document.links;
+// val = document.links[0];
+// val = document.links[0].id;
+// val = document.links[0].className;
+// val = document.links[0].classList[0];
 
-val = document.images;
+// val = document.images;
 
-val = document.scripts;
-val = document.scripts[2].getAttribute('src');
+// val = document.scripts;
+// val = document.scripts[2].getAttribute('src');
 
-let scripts = document.scripts;
+// let scripts = document.scripts;
 
-let scriptsArr = Array.from(scripts);
+// let scriptsArr = Array.from(scripts);
 
-scriptsArr.forEach(function(script) {
-  console.log(script.getAttribute('src'));
+// scriptsArr.forEach(function(script) {
+//   console.log(script.getAttribute('src'));
+// });
+
+// console.log(val);
+
+// let val;
+
+// val = document;
+// val = document.all;
+// val = document.all[0];
+
+
+//cosole.log(listItems);
+
+// document.getElementsByTagName
+
+let lis = document.getElementsByTagName('li');
+console.log(lis);
+console.log(lis[0]);
+lis[0].style.color = 'red';
+lis[3].textContent = 'Hello';
+
+// Conver HTML Collection into array
+
+lis = Array.from(lis);
+
+lis.reverse();
+
+lis.forEach(function(li, index){
+  console.log(li.className);
+  li.textcontent = 'Hello';
 });
 
-console.log(val);
+console.log(lis);
